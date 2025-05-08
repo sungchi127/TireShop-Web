ecc5f726a59af4480c53568d0c769867a01672bf
sungchi127 <sung891220@gmail.com>
2025-05-08 14:51:27 +0800

    modify repair-services

--- frontend/pages/repair-services.js
+++ frontend/pages/repair-services.js
@@ -3,21 +3,21 @@ import styles from '../styles/RepairServices.module.css';
 
 const servicesData = {
   leftColumn: [
-    { name: '機油更換', price: '400' },
-    { name: '變速箱油更換', price: '400' },
+    { name: '機油更換', price: '500' },
+    { name: '變速箱油更換', price: '600' },
     { name: '煞車油更換', price: '600' },
     { name: '失壓續跑胎拆裝平衡工資', price: '600' },
     { name: '輪胎拆裝平衡(16吋以下)', price: '400' },
-    { name: '輪胎拆裝平衡(17吋-19吋)', price: '400' },
+    { name: '輪胎拆裝平衡(17吋-19吋)', price: '500' },
     { name: '輪胎拆裝平衡(20吋以上)', price: '500' },
   ],
   rightColumn: [
-    { name: '3D四輪定位', price: '500' }, // 注意：此價格根據圖片辨識，首位數字較模糊
-    { name: '輪胎平衡', price: '400' },
+    { name: '3D四輪定位', price: '1500' }, // 注意：此價格根據圖片辨識，首位數字較模糊
+    { name: '輪胎平衡', price: '600' },
     { name: '外出工資', price: '300' },
     { name: '香菇頭補胎', price: '400' },
     { name: '失壓續跑胎香菇頭補胎', price: '600' },
-    { name: '補胎(內)', price: '200' },
+    { name: '補胎(內)', price: '300' },
     // { name: '項目七', price: '800' }, // 圖片中最後一項為空，價格800，暫不列出
   ]
 };
